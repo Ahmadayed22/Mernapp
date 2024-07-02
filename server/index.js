dfa02af7ee2b96b0263d554bd0046885ddabcb06
Ahmadayed22 <ahmad.ayed461@gmail.com>
2024-07-02 06:13:03 +0300

    Create test api

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express")
 require("dotenv").config()
 const mongoose = require('mongoose');
+const UserRouter = require("./routes/user.route");
 const URL = process.env.MONGO_URL
 const Port = process.env.PORT
 
 const app = express()
+
+app.use("/api/user", (UserRouter));
+
+
+
 mongoose.connect(URL)
     .then(() => {
 
