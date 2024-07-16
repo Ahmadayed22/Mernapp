bafb88c42724b36aa2430eb8997ea0ec434093f5
Ahmadayed22 <ahmad.ayed461@gmail.com>
2024-07-16 17:45:02 +0300

    add Sidebar dashboard

---
+++ Clint/src/Components/DashSidebar.jsx
@@ -0,0 +1,42 @@
+
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import { Sidebar } from "flowbite-react";
+import { HiTable, HiUser, } from "react-icons/hi";
+import { Link } from "react-router-dom";
+const DashSidebar = () => {
+    const location = useLocation()
+    const [tab, setTab] = useState("")
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const tabFormUrl = urlParams.get("tab")
+        // console.log(tabFormUrl)
+        if (tabFormUrl) {
+            setTab(tabFormUrl)
+        }
+    }, [location.search])
+
+
+
+    return (
+        <Sidebar className="w-full md:w-56">
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to="/dashboard?tab=profile">
+                        <Sidebar.Item href="#" active={tab === "profile"} icon={HiUser} label={"User"} labelColor="dark">
+                            Users
+                        </Sidebar.Item>
+                    </Link>
+
+                    <Sidebar.Item icon={HiTable} className="cursor-pointer">
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    );
+}
+
+
+
+export default DashSidebar
--- Clint/src/pages/Dashboard.jsx
+++ Clint/src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../Components/DashSidebar"
+import DashProfile from "../Components/DashProfile"
 
 const Dashboard = () => {
+    const location = useLocation()
+    const [tab, setTab] = useState("")
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const tabFormUrl = urlParams.get("tab")
+        // console.log(tabFormUrl)
+        if (tabFormUrl) {
+            setTab(tabFormUrl)
+        }
+    }, [location.search])
+
     return (
-        <div>Dashboard</div>
+        <div className="flex flex-col md:flex-row min-h-screen">
+            {/* Side bar */}
+            <div className="md:w-56">
+                <DashSidebar />
+            </div>
+            {/* Profile */}
+            <div className="flex-1">
+                {tab === "profile" ? <DashProfile /> : (null)}
+
+            </div>
+        </div>
     )
 }
 
