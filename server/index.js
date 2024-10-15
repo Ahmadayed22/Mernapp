845ea5b670bb8e2d6a530816812d67acbe8942f4
Ahmadayed22 <ahmad.ayed461@gmail.com>
2024-10-16 00:23:50 +0300

    finish project and add pre installation

--- server/index.js
+++ server/index.js
@@ -1,36 +1,43 @@
-const express = require("express")
-require("dotenv").config()
+const express = require("express");
+require("dotenv").config();
 const mongoose = require('mongoose');
 const UserRouter = require("./routes/user.route");
 const AuthRouter = require("./routes/auth.route");
-const PostRouter = require("./routes/Post.route")
-const CommentRouter = require("./routes/comment.route")
+const PostRouter = require("./routes/Post.route");
+const CommentRouter = require("./routes/comment.route");
 const cookieParser = require("cookie-parser");
-const URL = process.env.MONGO_URL
-const Port = process.env.PORT
-const cors = require("cors")
-const app = express()
+const path = require('path');
+const URL = process.env.MONGO_URL;
+const Port = process.env.PORT;
+const cors = require("cors");
+
+const app = express();
+
 app.use(cors({
     origin: 'http://localhost:5173', // Frontend origin
-    credentials: true ,// Enable cookies and other credentials in CORS
-    // optionSuccessStatus: 200,
+    credentials: true, // Enable cookies and other credentials in CORS
 }));
-app.use(cookieParser())
-app.use(express.json())
-app.use("/api/auth",AuthRouter)
-app.use("/api/user", (UserRouter));
-app.use("/api/post",PostRouter)
-app.use("/api/comment",CommentRouter)
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", AuthRouter);
+app.use("/api/user", UserRouter); // Removed extra parentheses
+app.use("/api/post", PostRouter);
+app.use("/api/comment", CommentRouter);
+
+// Use __dirname directly without re-declaration
+app.use(express.static(path.join(__dirname, 'Clint', 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Clint', 'dist', 'index.html'));
+});
 
 mongoose.connect(URL)
-    .then(() => {
-
+  .then(() => {
     app.listen(Port, () => {
-        console.log('we are listening on port ' ,Port ,'& Connecting to Db')
-})
-    })
-    .catch((err) => {
-        console.log("The error is : " ,err)
-    })
-
-
+      console.log('we are listening on port', Port, '& Connecting to Db');
+    });
+  })
+  .catch((err) => {
+    console.log("The error is:", err);
+  });
